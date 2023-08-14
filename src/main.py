782f714906767f557b0305882d940f0ae93b1669
danczw <dev.dc@pm.me>
2023-08-14 20:59:04 +0200

    add train func

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import matplotlib.pyplot as pl
 import numpy as np
@@ -95,7 +96,7 @@ def decode(ind_list: list, ind_char_map: dict) -> str:
 
 
 def get_batches(
-    data: torch.tensor,
+    data: torch.Tensor,
     context_window: int,
     split: str = "train",
     train_size: float = 0.8,
@@ -114,24 +115,21 @@ def get_batches(
         tuple: tuple of (x, y) where x is the input and y is the target
     """
     # define validation and test size in percentage
+    data_size = len(data)
     val_size = (1 - train_size) / 2
     test_size = 1 - train_size - val_size
-    data_size = len(data)
-
-    logging.info(f"Train size: {train_size}% - {int(data_size * train_size)}")
-    logging.info(f"Val size: {val_size}% - {int(data_size * val_size)}")
-    logging.info(f"Test size: {test_size}% - {int(data_size * test_size)}")
-    logging.info(f"Data size: {data_size}")
 
     # split data into train, val, test
     train = data[: int(data_size * train_size)]
     val = data[int(data_size * train_size) : int(data_size * (train_size + val_size))]
     test = data[int(data_size * (train_size + val_size)) :]
 
-    logging.info(f"Actual train size: {train.shape}")
-    logging.info(f"Actual val size: {val.shape}")
-    logging.info(f"Actual test size: {test.shape}")
+    logging.debug(f"Data size: {data_size}")
+    logging.debug(f"Train size: {train_size}% - {int(data_size * train_size)}")
+    logging.debug(f"Val size: {val_size}% - {int(data_size * val_size)}")
+    logging.debug(f"Test size: {test_size}% - {int(data_size * test_size)}")
 
+    # set batch data
     batch_data = train
     if split == "val":
         batch_data = val
@@ -147,7 +145,7 @@ def get_batches(
 
 
 @torch.no_grad()
-def evaluate_loss(model: torch.nn.Module, dataset: torch.tensor, config: dict) -> dict:
+def evaluate_loss(model: torch.nn.Module, dataset: torch.Tensor, config: dict) -> dict:
     """Evaluate loss on train and test set
 
     Args:
@@ -156,14 +154,16 @@ def evaluate_loss(model: torch.nn.Module, dataset: torch.tensor, config: dict) -
         config (dict): config dictionary
 
     Returns:
-        dict:
+        dict: dictionary of loss on train and test set
     """
     output = {}
     model.eval()
 
+    # evaluate loss on train and test set
     for split in ["train", "test"]:
         losses = []
         for _ in range(10):
+            # get batches and calculate loss for each batch
             xb, yb = get_batches(
                 data=dataset,
                 context_window=config["context_window"],
@@ -173,11 +173,81 @@ def evaluate_loss(model: torch.nn.Module, dataset: torch.tensor, config: dict) -
             )
             _, loss = model(xb, yb)
             losses.append(loss.item())
+        # calculate mean loss across batches
         output[split] = np.mean(losses)
 
-    model.train()
+    logging.debug(f"Loss on train set: {output['train']}")
+    logging.debug(f"Loss on test set: {output['test']}")
 
+    model.train()
     return output
+
+
+def train(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    dataset: torch.Tensor,
+    config: dict,
+    scheduler=None,
+) -> ps.DataFrame:
+    """Train model
+
+    Args:
+        model (torch.nn.Module): model to train
+        optimizer (torch.optim.Optimizer): optimizer
+        dataset (torch.tensor): dataset tensor of encoded data
+        config (dict): config dictionary
+        scheduler (torch.optim.lr_scheduler, optional): learning rate scheduler. Defaults to None.
+
+    Returns:
+        ps.DataFrame: dataframe of losses for each epoch
+    """
+    losses = []
+    logging.info(f"Start training for {config['epochs']} epochs")
+
+    for epoch in range(config["epochs"]):
+        start_time = time.time()
+
+        # reset gradients
+        optimizer.zero_grad()
+
+        # get batches
+        xs, ys = get_batches(
+            data=dataset,
+            context_window=config["context_window"],
+            split="train",
+            train_size=config["train_size"],
+            batch_size=config["batch_size"],
+        )
+
+        # forward pass
+        logits, loss = model(xs, ys)
+        # backward pass
+        loss.backwards()
+        # update parameters
+        optimizer.step()
+
+        if scheduler:
+            scheduler.step()
+
+        if epoch % config["log_interval"] == 0:
+            batch_time = time.time() - start_time
+            x = evaluate_loss(model=model, dataset=dataset, config=config)
+            losses += [x]
+
+            epoch_log = f"Epoch: {epoch}"
+            val_log = f"val loss {x['val']:.3f}"
+            time_log = f"time {batch_time:.3f}"
+            eta_log = f"ETA {batch_time * (config['epochs'] - epoch):.3f}"
+            logging.info(" | ".join([epoch_log, val_log, time_log, eta_log]))
+
+            if scheduler:
+                logging.info(f"Learning rate: {scheduler.get_lr()}")
+
+    logging.info(f"Training completed for {config['epochs']} epochs")
+    logging.info(f"Final loss: {losses[-1]}")
+
+    return ps.DataFrame(losses)
 
 
 def main():
