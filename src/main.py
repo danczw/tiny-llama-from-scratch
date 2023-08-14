9bba0292c03629d76256f4f0773e93bdbcae195d
danczw <dev.dc@pm.me>
2023-08-14 18:42:04 +0200

    add data load

--- src/main.py
+++ src/main.py
@@ -33,3 +33,36 @@ def load_config(path: str) -> dict:
         exit(1)
 
     return config
+
+
+def load_data(path: str) -> tuple:
+    """Load txt file and return
+
+    Args:
+        path (str): path to txt file
+
+    Returns:
+        tuple: tuple of (ind_to_char, char_to_ind, vocab, lines)
+            with ind_to_char: dict of index to character
+            with char_to_ind: dict of character to index
+            with vocab: list of unique characters
+            with lines: string of all characters
+    """
+    try:
+        logging.info(f"Loading data from {path}")
+
+        lines = open(path, "r").read()
+        logging.info(f"Loaded {len(lines)} characters")
+
+        vocab = sorted(list(set(lines)))
+        logging.info(f"Loaded {len(vocab)} unique characters")
+
+    except Exception as e:
+        logging.error(f"Error loading data at path {path}: {e}")
+        exit(1)
+
+    # create a mapping of unique chars to integers
+    ind_to_char = {i: ch for i, ch in enumerate(vocab)}
+    char_to_ind = {ch: i for i, ch in enumerate(vocab)}
+
+    return ind_to_char, char_to_ind, vocab, lines
